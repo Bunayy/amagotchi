fa29a2cc923d0f49a73b97b0cb84fb45af82449e
Bunayy <bunayya@hotmail.de>
2015-09-09 00:35:24 +0200

    test

---
+++ Amagotchi/app/src/main/java/projekt/fhflensburg/amagotchi/Amagotchi.java
@@ -0,0 +1,19 @@
+package projekt.fhflensburg.amagotchi;
+
+/**
+ * Created by bunay_000 on 09.09.2015.
+ */
+public class Amagotchi
+{
+    private String name;
+    private double health;
+    private double repletion;
+    private double sleep;
+    private double motivation;
+    private double happyness;
+
+    public Amagotchi()
+    {
+
+    }
+}
