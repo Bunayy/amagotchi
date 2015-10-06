29b9ab034699284ff1a0c8ffbed9b64916a5c77f
Martin Steffen <m.steffen1337@yahoo.de>
2015-10-06 08:51:51 +0200

    MainLoop

--- Amagotchi/app/src/main/java/projekt/fhflensburg/amagotchi/MainService.java
+++ Amagotchi/app/src/main/java/projekt/fhflensburg/amagotchi/MainService.java
@@ -9,6 +9,8 @@ import android.os.Message;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Random;
+
 /**
  * Created by Howie on 05.10.2015.
  */
@@ -16,12 +18,15 @@ public class MainService extends Service
 {
     private static final String LOG_TAG = "MainService";
     private IBinder mBinder = new MyBinder();
+    Random r = new Random();
 
     static Handler handler;
 
     //Daten
     static Amagotchi ama = null;
     static Boolean run = false;
+
+    int loopCount = 0;
 
     @Override
     public void onCreate() {
@@ -121,26 +126,148 @@ public class MainService extends Service
         double fitnessThisPeriod = -0.12;
         double attentionThisPeriod = -0.28;
 
+        //Alte Werte merken
+        double oldAttention = 0.0;
+
         if(ama != null && run)
         {
-            //SChalfen, ShitLiegtRum
-            if(ama.getIsAsleep())
+            oldAttention = ama.getAttention();
+
+            if(ama.getIsDead() == false)
             {
-                sleepThisPeriod += 0.2;
+                if (ama.getLevel() == 0) {
+                    ////////////////////////////////////////
+                    // Hatching
+                    ////////////////////////////////////////
+
+                    if (ama.getTimeToHatch() > 1) {
+                        ama.setTimeToHatch(ama.getTimeToHatch() - 1);
+                    } else {
+                        ama.setLevel(1);
+                    }
+                } else {
+                    ////////////////////////////////////////
+                    // This Period
+                    ////////////////////////////////////////
+
+                    //Feces Countdown
+                    if (ama.getFecesCountdown() > 1) {
+                        ama.setFecesCountdown(ama.getFecesCountdown() - 1);
+                    } else if (ama.getFecesCountdown() == 1) {
+                        ama.setIsSickInfection(true);
+                        healthThisPeriod -= 30;
+                        motivationThisPeriod -= 15;
+                        ama.setFecesCountdown(0);
+                    }
+
+                    //Cure from overeating
+                    if (ama.getIsSickOveraeting() && ama.getRepletion() <= 100) {
+                        ama.setIsSickOveraeting(false);
+                    }
+
+                    //Schläft
+                    if (ama.getIsAsleep()) {
+                        sleepThisPeriod += 0.2;
+                    }
+
+                    //Fäkalien liegen rum
+                    if (ama.getFeces()) {
+                        happinessThisPeriod -= 0.085;
+                    }
+
+                    //Überfressen
+                    if (ama.getIsSickOveraeting()) {
+                        healthThisPeriod -= 0.085;
+                        happinessThisPeriod -= 0.25;
+                    }
+
+                    //Infektion
+                    if (ama.getIsSickInfection()) {
+                        healthThisPeriod -= 0.17;
+                        motivationThisPeriod -= 0.125;
+                        happinessThisPeriod -= 0.17;
+                    }
+
+                    //Sättigung
+                    if (ama.getRepletion() == 0) {
+                        healthThisPeriod -= 0.34;
+                        motivationThisPeriod -= 0.125;
+                        happinessThisPeriod -= 0.17;
+                        attentionThisPeriod -= 25;
+                    } else if (ama.getRepletion() <= 20) {
+                        healthThisPeriod -= 0.17;
+                        motivationThisPeriod -= 0.125;
+                        happinessThisPeriod -= 0.17;
+                    } else if (ama.getRepletion() <= 40) {
+                        motivationThisPeriod -= 0.125;
+                    }
+
+                    //Schlaf
+                    if (ama.getSleep() == 0) {
+                        healthThisPeriod -= 0.34;
+                        motivationThisPeriod -= 25;
+                        happinessThisPeriod -= 0.17;
+                    } else if (ama.getSleep() <= 10) {
+                        healthThisPeriod -= 0.17;
+                        motivationThisPeriod -= 25;
+                        happinessThisPeriod -= 0.17;
+                    } else if (ama.getSleep() <= 20) {
+                        motivationThisPeriod -= 25;
+                        happinessThisPeriod -= 0.17;
+                    }
+
+                    //Fitness
+                    if (ama.getFitness() <= 20) {
+                        healthThisPeriod -= 0.021;
+                    } else if (ama.getFitness() >= 80) {
+                        healthThisPeriod -= 0.042;
+                    }
+
+                    ////////////////////////////////////////
+                    // Berechnung
+                    ////////////////////////////////////////
+
+                    ama.setHealth(ama.getHealth() + healthThisPeriod);
+                    ama.setRepletion(ama.getRepletion() + repletionThisPeriod);
+                    ama.setSleep(ama.getSleep() + sleepThisPeriod);
+                    ama.setMotivation(ama.getMotivation() + motivationThisPeriod);
+                    ama.setHappiness(ama.getHappiness() + happinessThisPeriod);
+                    ama.setFitness(ama.getFitness() + fitnessThisPeriod);
+                    ama.setAttention(ama.getAttention() + attentionThisPeriod);
+
+
+                    //Aufmerksamkeit
+
+                    //loopCount
+
+
+                    //Gamble Feces
+                    if (ama.getAge() % 60 == 0 && !ama.getFeces()) {
+                        Log.v(LOG_TAG, "PoopGamble");
+
+                        if (r.nextInt(4) == 2) {
+                            ama.setFeces(true);
+                            ama.setFecesCountdown(180);
+                        }
+                    }
+
+                    //Gamble Overeating
+                    if (ama.getRepletion() >= 120) {
+                        ama.setIsSickOveraeting(true);
+                    } else if (ama.getRepletion() >= 110) {
+                        if (r.nextInt(10) > 7) {
+                            ama.setIsSickOveraeting(true);
+                        }
+                    } else if (ama.getRepletion() >= 105) {
+                        if (r.nextInt(4) == 1) {
+                            ama.setIsSickOveraeting(true);
+                        }
+                    }
+
+                }
+                //Altern
+                ama.setAge(ama.getAge() + 1);
             }
-
-            if(ama.getFeces())
-            {
-
-            }
-
-
-
-
-
-            /*ama.setMotivation(ama.getMotivation() + 0.1);
-            //Log.v(LOG_TAG, "MotIs: " + ama.getMotivation());
-            //Log.v(LOG_TAG, "Saving");*/
 
             Sys.saveGame(ama, this);
             MainActivity.instance.updateInterface();
